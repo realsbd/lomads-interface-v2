172b0f82c78c061d9b7a9a62068157e4b13c8a15
zohaibkibria <zohaib.k@cloudbloom.io>
2023-06-19 13:29:11 +0530

    updated treasury

--- src/pages/Dashboard/Treasury/components/CreditDebit.tsx
+++ src/pages/Dashboard/Treasury/components/CreditDebit.tsx
@@ -5,11 +5,12 @@ import DebitExecutedSvg from 'assets/svg/sendToken.svg'
 import CreditSvg from 'assets/svg/receiveToken.svg'
 
 export default ({ credit = false, executed = false, amount, token }: any) => {
+    console.log("token : ", token)
     return (
         <TableCell>
             <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                { !(isNaN(amount) || !token) ? <img style={{ width: 24, height: 24 }} src={credit ? CreditSvg : executed ? DebitExecutedSvg : DebitSvg } /> : <Box sx={{ width: 24, height: 24 }}></Box> }
-                <Typography style={{ fontFamily: 'Inter,sans-serif', fontWeight: 700, fontSize: 14, color: '#76808d',  }} sx={{ width: 50 }} ml={1}>{ isNaN(amount) || !token ? '-' : `${parseFloat(amount).toFixed(3)} ${token}` }</Typography>
+                {!(isNaN(amount) || !token) ? <img style={{ width: 24, height: 24 }} src={credit ? CreditSvg : executed ? DebitExecutedSvg : DebitSvg} /> : <Box sx={{ width: 24, height: 24 }}></Box>}
+                <Typography style={{ fontFamily: 'Inter,sans-serif', fontWeight: 700, fontSize: 14, color: '#76808d', }} sx={{ width: 50 }} ml={1}>{isNaN(amount) || !token ? '-' : `${parseFloat(amount).toFixed(3)} ${token}`}</Typography>
             </Box>
         </TableCell>
     )
--- src/pages/Dashboard/Treasury/components/Row.tsx
+++ src/pages/Dashboard/Treasury/components/Row.tsx
@@ -19,7 +19,7 @@ export default ({ transaction, executableNonce }: any) => {
     const { loadSafe } = useSafe()
 
     const safeChainId = useMemo(() => {
-        if(DAO?.url)
+        if (DAO?.url)
             return _find(DAO?.safes, s => s.address === transaction?.safeAddress)?.chainId
         return undefined
     }, [DAO?.url])
@@ -35,55 +35,55 @@ export default ({ transaction, executableNonce }: any) => {
         for (let index = 0; index < txn.length; index++) {
             const tx = txn[index];
             const metadata = _get(transaction, `metadata.${tx.to}`)
-            if(!tx.to || tx.to === "0x") return;
-            let actions :any = {}
-            if(metadata?.sweatConversion) {
+            if (!tx.to || tx.to === "0x") return;
+            let actions: any = {}
+            if (metadata?.sweatConversion) {
                 // reset sweat for recipient && update user earnings
-                actions = { 
-                    ...actions, 
-                    "RESET_SWEAT": { user: tx?.to, daoId: DAO?._id, }, 
-                    "UPDATE_EARNING": { user: tx?.to, daoId: DAO?._id, symbol: tx?.symbol, value: +tx?.formattedValue, currency: tx?.tokenAddress || 'SWEAT' } 
+                actions = {
+                    ...actions,
+                    "RESET_SWEAT": { user: tx?.to, daoId: DAO?._id, },
+                    "UPDATE_EARNING": { user: tx?.to, daoId: DAO?._id, symbol: tx?.symbol, value: +tx?.formattedValue, currency: tx?.tokenAddress || 'SWEAT' }
                 }
-            } else if(metadata?.taskId) {
+            } else if (metadata?.taskId) {
                 // close task && update payment for user
-                actions = { 
-                    ...actions, 
-                    "TASK_PAID": { taskId: metadata?.taskId, user: tx?.to }, 
-                    "UPDATE_EARNING": { user: tx?.to, daoId: DAO?._id, symbol: tx?.symbol, value: +tx?.formattedValue, currency: tx?.tokenAddress || 'SWEAT' } 
+                actions = {
+                    ...actions,
+                    "TASK_PAID": { taskId: metadata?.taskId, user: tx?.to },
+                    "UPDATE_EARNING": { user: tx?.to, daoId: DAO?._id, symbol: tx?.symbol, value: +tx?.formattedValue, currency: tx?.tokenAddress || 'SWEAT' }
                 }
             } else {
                 // update user earnings
-                actions = { 
-                    ...actions, 
-                    "UPDATE_EARNING": { user: tx?.to, daoId: DAO?._id, symbol: tx?.symbol, value: +tx?.formattedValue, currency: tx?.tokenAddress || 'SWEAT' } 
+                actions = {
+                    ...actions,
+                    "UPDATE_EARNING": { user: tx?.to, daoId: DAO?._id, symbol: tx?.symbol, value: +tx?.formattedValue, currency: tx?.tokenAddress || 'SWEAT' }
                 }
             }
             actionList.push(actions)
         }
-        if(actionList.length > 0) {
-              await axiosHttp.post(`gnosis-safe/${transaction?.rawTx?.safeTxHash}/executed`, actionList)  
-              .then(res => {
-                console.log(res.data)
-              })
+        if (actionList.length > 0) {
+            await axiosHttp.post(`gnosis-safe/${transaction?.rawTx?.safeTxHash}/executed`, actionList)
+                .then(res => {
+                    console.log(res.data)
+                })
         }
     }
 
-    if(transaction?.rawTx?.daoId && transaction?.rawTx?.token?.tokenAddress === 'SWEAT' && transaction?.rawTx?.daoId !== DAO?._id)
-        return null
-    
-    if(transaction?.daoId && transaction?.rawTx?.token?.tokenAddress === 'SWEAT' && transaction?.daoId !== DAO?._id)
+    if (transaction?.rawTx?.daoId && transaction?.rawTx?.token?.tokenAddress === 'SWEAT' && transaction?.rawTx?.daoId !== DAO?._id)
         return null
 
-    if(!txn) return null
+    if (transaction?.daoId && transaction?.rawTx?.token?.tokenAddress === 'SWEAT' && transaction?.daoId !== DAO?._id)
+        return null
+
+    if (!txn) return null
 
     return (
         <>
             {
                 txn.map((tx: any, _i: number) => (
                     <TableRow>
-                        <CreditDebit credit={tx?.isCredit} executed={tx?.executionDate} amount={tx?.formattedValue} token={tx?.symbol}/>
-                        <Label transaction={transaction} recipient={tx?.to}/>
-                        <Recipient safeAddress={transaction?.safeAddress} credit={tx?.isCredit} recipient={tx?.to} />
+                        <CreditDebit credit={tx?.isCredit} executed={tx?.executionDate} amount={tx?.formattedValue} token={tx?.symbol} />
+                        <Label transaction={transaction} recipient={tx?.to} />
+                        <Recipient safeAddress={transaction?.safeAddress} credit={tx?.isCredit} recipient={tx?.to} token={tx?.symbol} />
                         <Tag transaction={transaction} recipient={tx?.to} />
                         <Sign transaction={tx} index={_i} />
                         <Action txnGroup={txn} amount={tx?.formattedValue} onPostExecution={handlePostExecution} token={tx?.symbol} tokenAddress={tx?.tokenAddress} executableNonce={executableNonce} safeAddress={transaction?.safeAddress} transaction={tx} txnCount={txn.length} chainId={loadSafe(transaction?.safeAddress)?.chainId} index={_i} />
