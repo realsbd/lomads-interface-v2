071748e75d680ee69f947646b94ff690b55f21e1
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-06-27 17:52:01 +0530

    bug fix

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -88,7 +88,7 @@ export type SupportedL2ChainId = typeof L2_CHAIN_IDS[number]
 
 export const GNOSIS_SAFE_BASE_URLS:any = {
   [SupportedChainId.MAINNET]: 'https://safe-transaction-mainnet.safe.global',
-  [SupportedChainId.GOERLI]: 'https://safe-transaction.goerli.gnosis.io',
+  [SupportedChainId.GOERLI]: 'https://safe-transaction-goerli.safe.global',
   [SupportedChainId.POLYGON]: 'https://safe-transaction-polygon.safe.global'
 }
 
