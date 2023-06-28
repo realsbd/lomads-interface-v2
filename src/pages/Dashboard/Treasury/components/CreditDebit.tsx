6a1abfd762400987ae29acacffdc096ffa0464c6
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-06-28 20:31:18 +0530

    bug fix

--- src/pages/Dashboard/Treasury/components/CreditDebit.tsx
+++ src/pages/Dashboard/Treasury/components/CreditDebit.tsx
@@ -10,7 +10,7 @@ export default ({ credit = false, executed = false, amount, token, fiatConversio
         <TableCell>
             <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
                 {!(isNaN(amount) || !token) ? <img style={{ width: 24, height: 24 }} src={credit ? CreditSvg : executed ? DebitExecutedSvg : DebitSvg} /> : <Box sx={{ width: 24, height: 24 }}></Box>}
-                <Typography style={{ fontFamily: 'Inter,sans-serif', fontWeight: 700, fontSize: 14, color: '#76808d', }} sx={{ width: 50 }} ml={1}>{isNaN(amount) || !token ? '-' : `${parseFloat(amount).toFixed(3)} ${token}`} <span style={{ fontWeight: 400, fontSize: 10, color: "#76808D" }}>{ fiatConversion ? `($${ (+amount * +fiatConversion).toFixed(3) })` : '' }</span></Typography>
+                <Typography style={{ fontFamily: 'Inter,sans-serif', whiteSpace: 'nowrap', fontWeight: 700, fontSize: 14, color: '#76808d', }}  ml={1}>{isNaN(amount) || !token ? '-' : `${parseFloat(amount).toFixed(2)} ${token}`} <br/><span style={{ fontWeight: 400, fontSize: 10, color: "#76808D" }}>{ fiatConversion ? `${ (+amount * +fiatConversion).toFixed(3) } USD` : '' }</span></Typography>
             </Box>
         </TableCell>
     )
