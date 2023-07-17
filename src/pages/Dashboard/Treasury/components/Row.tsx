e77e60e7de6206a2e2e073586c9b18b8fb9b99be
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-07-17 17:46:56 +0530

    bug fix

--- src/pages/Dashboard/Treasury/components/Row.tsx
+++ src/pages/Dashboard/Treasury/components/Row.tsx
@@ -54,7 +54,7 @@ export default ({ transaction, executableNonce }: any) => {
                 // close task && update payment for user
                 actions = {
                     ...actions,
-                    "TASK_PAID": { taskId: metadata?.taskId || transaction?.rawTx?.taskId, user: tx?.to },
+                    "TASK_PAID": { taskId: metadata?.taskId ||  transaction?.rawTx?.taskId, user: tx?.to },
                     "UPDATE_EARNING": { user: tx?.to, daoId: DAO?._id, symbol: tx?.symbol, value: +tx?.formattedValue, currency: tx?.tokenAddress || 'SWEAT' },
                     "UPDATE_FIAT_CONVERSION": { txId: transaction?._id, recipient: tx?.to, fiatConversion: token?.fiatConversion }
                 }
