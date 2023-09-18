827c7d84e6aafde5552cf74b7f96669d0ad4c284
Lomads Technologies <107495745+lomads@users.noreply.github.com>
2023-09-18 10:10:06 +0200

    Update index.tsx

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -82,6 +82,7 @@ const useStyles = makeStyles((theme: any) => ({
         lineHeight: '42px !important',
         textTransform: 'uppercase',
         color: '#1B2B41',
+	textAlign: 'center',
     },
 }));
 
@@ -302,7 +303,7 @@ else {
                 </Box>
                 <Box sx={{ padding: '0 30px' }}>
                     <Typography className={classes.subtitle1}>Lomads app needs a PC<br />for now.</Typography>
-                    <Typography className={classes.subtitle2} sx={{ fontWeight: '800' }}>CATCH YOU ON<br />THE <span style={{ fontWeight: '300', fontStyle: 'italic', color: '#C94B32' }}>BIG SCREEN</span></Typography>
+                    <Typography className={classes.subtitle2} sx={{ fontWeight: '800' }}>CATCH YOU<br /> ON THE <span style={{ fontWeight: '300', fontStyle: 'italic', color: '#C94B32' }}><br />BIG SCREEN</span></Typography>
                 </Box>
             </Grid>
         </Grid>
